bee0973dbca68cb68806da64d3b23d868b30573c
Mathias Picker <mattis123@live.no>
2022-05-15 19:32:06 +0200

    Added hook

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,19 @@
-<script lang="ts">
+<script lang="ts" context="module">
 	import type { Load } from '@sveltejs/kit';
 
-	export const load: Load = async function () {
-		return {};
+	export const load: Load = async function ({session}) {
+		return {
+            props: {
+                foo: session.foo
+            }
+        };
 	};
 </script>
+
+<script lang="ts">
+export let foo: 'bar';
+</script>
+
+From load: {foo}
 
 <slot />
