c5f6b837ad1afab4fe5729c5d818a5ba560237dd
Mathias Picker <mattis123@live.no>
2022-05-15 21:01:17 +0200

    Still looking

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+import { read } from 'xlsx';
+
+console.log(read)
+</script>
+
 <h1>Adapter bug reproduction</h1>
 
 You can probably see me in dev (even in preview), but not in production.
