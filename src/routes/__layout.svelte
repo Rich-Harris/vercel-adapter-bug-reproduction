029bb62de09fa97fc96a4b61758af056e15950f4
Mathias Picker <mattis123@live.no>
2022-05-15 19:57:00 +0200

    Added endpoint

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts" context="module">
 	import type { Load } from '@sveltejs/kit';
 
-	export const load: Load = async function ({session}) {
+	export const load: Load = async function ({fetch, session}) {
+        const body = fetch('/test', { method: 'GET' })
+        const result = (await body).json()
 		return {
             props: {
-                foo: session.foo
+                foo: session.foo,
+                thing: result
             }
         };
 	};
@@ -12,8 +15,10 @@
 
 <script lang="ts">
 export let foo: 'bar';
+export let thing: string;
 </script>
 
 From load: {foo}
+From endpoint: {thing}
 
 <slot />
